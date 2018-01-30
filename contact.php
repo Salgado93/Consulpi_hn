5c3091d5ff24d61fd1c567d8bb5377c685692567
Salgado93 <danielsalgado@unitec.edu>
2018-01-30 06:57:33 -0600

    First Commit
    Respaldo Pagina Consulpihn

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+$para ='ingenieraivonne@hotmail.com';
+
+$asunto = "Mensaje desde la pagina Consulpi";
+$mailheader = "From: ".$_POST["email"]."\r\n";
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
+$mailheader .= "Content-type: text/html; charset=utf-8\r\n";
+$MESSAGE_BODY = "Nombre: ".$_POST["name"]."\n";
+$MESSAGE_BODY .= "\n<br>Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY .= "\n<br>Mensaje: ".nl2br($_POST["message"])."\n";
+
+mail($para,$asunto,$MESSAGE_BODY,$mailheader) or die("Error al enviar email");
+
+header('Location: http://consulpihn.com/');
+
+
+
+?>
